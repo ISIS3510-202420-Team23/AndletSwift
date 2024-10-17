607e64976c785dd4c73a4f6cccfcb92443c5f423
storres21 <88690048+storres21@users.noreply.github.com>
2024-10-16 23:18:36 -0500

    [Fix bugs] landlords offer view ready, filters and photos

--- Andlet/Andlet/Views/Helpers/OfferImageCarouselView.swift
+++ Andlet/Andlet/Views/Helpers/OfferImageCarouselView.swift
@@ -1,29 +1,21 @@
-//
-//  OfferDetailView.swift
-//  SwiftApp
-//
-//  Created by Sofía Torres Ramírez on 16/09/24.
-//
-
 import SwiftUI
-import FirebaseStorage
 
 struct OfferImageCarouselView: View {
     let property: PropertyModel
-    @State private var imageURLs: [URL] = []
+    @State private var images: [UIImage?] = []
     
     var body: some View {
         TabView {
-            if imageURLs.isEmpty {
+            if images.isEmpty {
                 // Placeholder cuando no hay imágenes
                 Text("No images available")
             } else {
-                ForEach(imageURLs, id: \.self) { url in
-                    AsyncImage(url: url) { image in
-                        image
+                ForEach(Array(images.enumerated()), id: \.0) { index, image in
+                    if let image = image {
+                        Image(uiImage: image)
                             .resizable()
                             .scaledToFill()
-                    } placeholder: {
+                    } else {
                         // Placeholder mientras se carga la imagen
                         ProgressView()
                     }
@@ -37,37 +29,15 @@ struct OfferImageCarouselView: View {
     }
     
     func loadImages() {
-        let storage = Storage.storage()
-        var urls: [URL] = []
+        var tempImages: [UIImage?] = Array(repeating: nil, count: property.photos.count)
         
-        // Iterar sobre las rutas de las imágenes en el array "photos"
-        for photo in property.photos {
-            let storageRef = storage.reference().child("properties/\(photo)")
-            
-            // Obtener la URL de descarga
-            storageRef.downloadURL { url, error in
-                if let error = error {
-                    // Manejo de errores
-                    print("Error al obtener la URL de la imagen \(photo): \(error.localizedDescription)")
-                    return
-                }
-                
-                if let url = url {
-                    urls.append(url)
-                    
-                    // Actualizar el estado en el hilo principal
-                    DispatchQueue.main.async {
-                        imageURLs = urls
-                    }
-                } else {
-                    print("Error: No se pudo obtener la URL para la imagen \(photo)")
+        for (index, photo) in property.photos.enumerated() {
+            ImageCacheManager.shared.getImage(forKey: photo) { image in
+                DispatchQueue.main.async {
+                    tempImages[index] = image
+                    self.images = tempImages
                 }
             }
         }
     }
-    
 }
-
-//#Preview{
-//    OfferImageCarouselView()
-//}
